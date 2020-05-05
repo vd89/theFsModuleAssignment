760a15b72bc9eac2437ad13d6046a0c7cb9930c2
vd89 <me@daxcodes.in>
2020-05-05 06:25:33 +0530

    creating the new file and converting the data to encrypted

--- app.js
+++ app.js
@@ -7,26 +7,30 @@ Algorithm
 
 const readlineSync = require('readline-sync');
 const fs = require('fs');
-const crypt = require('./encrypt')
+const crypt = require('./encrypt');
 
-const fileName = readlineSync
-  .question('Please provide the file names: ')
+const fileName = readlineSync.question('Please provide the file names: ');
 
-
-
-if (fileName !== "") {
-    const key = readlineSync.question('Please provide the Key for the Encryption: ')
-    console.log(key);
-    // console.log(fileName.split(' '));
-    fs.readFile(fileName, (err, data) => {
-        if (err) {
-            throw err
-        }
-        const nData = data.toString()
-        const newData = crypt(nData, key)
-        console.log(newData);
-    } )
+if (fileName !== '') {
+  const key = readlineSync.question(
+    'Please provide the Key for the Encryption: ',
+  );
+  console.log(key);
+  // console.log(fileName.split(' '));
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const nData = data.toString();
+    const newData = crypt(nData, key);
+    // console.log(newData);
+    fs.writeFile('newFile.txt', newData, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`The file is created with encrypted Key 😸 😸`);
+    });
+  });
 } else {
-    console.log(`Please prvide the file name to encrypt 🔥 ✊`);
+  console.log(`Please prvide the file name to encrypt 🔥 ✊`);
 }
-
