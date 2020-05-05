acb63610bf886bd40614e65c6e689b154af537e0
vd89 <me@daxcodes.in>
2020-05-05 08:03:43 +0530

    encrpyt and decrypt the given file with the name for each file

--- app.js
+++ app.js
@@ -9,28 +9,23 @@ const readlineSync = require('readline-sync');
 const fs = require('fs');
 const crypt = require('./encrypt');
 
-const fileName = readlineSync.question('Please provide the file names: ');
-
-if (fileName !== '') {
-  const key = readlineSync.question(
-    'Please provide the Key for the Encryption: ',
+const que = () => {
+  const forEncrDecr = readlineSync.question(
+    `Please provide the purpose encrypt('en') or decrypt('de') : `,
   );
-  console.log(key);
-  // console.log(fileName.split(' '));
-  fs.readFile(fileName, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    const nData = data.toString();
-    const newData = crypt(nData, key);
-    // console.log(newData);
-    fs.writeFile('newFile.txt', newData, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`The file is created with encrypted Key 😸 😸`);
-    });
-  });
-} else {
-  console.log(`Please prvide the file name to encrypt 🔥 ✊`);
-}
+  if (forEncrDecr === 'en') {
+    const fileName = readlineSync.question(
+      'Please provide the file names to encrypt: ',
+    );
+    crypt.forEncrypt(fileName);
+    return;
+  } else {
+    const fileName = readlineSync.question(
+      'Please provide the file names to decrypt: ',
+    );
+    crypt.forDecrypt(fileName);
+    return;
+  }
+};
+
+que()
--- encrypt.js
+++ encrypt.js
@@ -1,3 +1,6 @@
+const readlineSync = require('readline-sync');
+const fs = require('fs');
+
 const crypt = (input, ke) => {
   var output = [];
   var key = ke;
@@ -10,4 +13,56 @@ const crypt = (input, ke) => {
   return output.join('');
 };
 
-module.exports = crypt
+const forEncrypt = (fName) => {
+  if (fName !== '') {
+    const key = readlineSync.question(
+      'Please provide the Key for the Encryption: ',
+    );
+    fs.readFile(fName, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const nData = data.toString();
+      const newData = crypt(nData, key);
+      fs.writeFile(`${key}.en${fName}`, newData, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(
+          `The file is created with encrypted Key '${key}.en${fName}' is the file 😸 😸`,
+        );
+      });
+    });
+  } else {
+    console.log(`Please prvide the file name to encrypt 🔥 ✊`);
+  }
+};
+
+const forDecrypt = (fName) => {
+  if (fName !== '') {
+    const key = fName.split('.')[0];
+    const newFileName = fName.split('.')[1];
+    fs.readFile(fName, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const nData = data.toString();
+      const newData = crypt(nData, key);
+      fs.writeFile(`de${newFileName}.txt`, newData, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(
+          `The file is created with 'de${newFileName}.txt' with decryption 😃 😃`,
+        );
+      });
+    });
+  } else {
+    console.log(`Please provide the File name to Decrypt ✊ 🔥 🔥`);
+  }
+};
+module.exports = {
+  crypt,
+  forEncrypt,
+  forDecrypt,
+};
